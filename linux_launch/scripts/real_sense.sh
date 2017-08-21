dc140119e9d2a6dc957e7996a06d0ef5919d4e67
jingzongfei <jingzongfei@irobsen.com>
2017-08-21 13:22:10 +0800

    add the scripts files

---
+++ linux_launch/scripts/real_sense.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /opt/ros/kinetic/setup.bash
+source /home/vincent/Project/path_planning/devel/setup.bash
+
+export ROS_MASTER_URI=http://127.0.0.1:11311/
+export ROS_IP=127.0.0.1
+
+rosrun monitor real_sense
---
+++ linux_launch/scripts/roscore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /opt/ros/kinetic/setup.bash
+source /home/vincent/Project/path_planning/devel/setup.bash
+
+export ROS_MASTER_URI=http://127.0.0.1:11311/
+export ROS_IP=127.0.0.1
+roscore
